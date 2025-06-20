cb62f32811dcef89122acb230922494fb81105f0
pablothecoder <ortegaperaltap@gmail.com>
2025-06-20 10:58:20 -0400

    html css and js files updated

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
+// DOM variables
+
 const body = document.querySelector('body')
+const searchShowForm = document.querySelector('#headerSearchForm')
 const searchShowBtn = document.querySelector('#searchBtn')
 const searchInput = document.querySelector('#searchShowInput')
 const mainContainer = document.querySelector('.main-container')
 const featuredSection = document.querySelector('.featured-today')
+const featuredSectionShows = document.querySelector('featured-today-shows')
+const searchResultSection = document.querySelector('search-results')
+const searchResults = document.querySelector('#searchResultShows')
 const premiereSection = document.querySelector('.premieres-annoucements')
 
-// New elements
+// TV API functions
 
-const newP = document.createElement('p')
-const newImg = document.createElement('img')
-
-// TV API 
-
-const url = 'https://api.tvmaze.com/search/shows'
-
-const fetchTv = async () => {
+const GetTvShows = async (searhInput) => {
     try {
-        const res = await axios.get(url)
-        console.log(res)
+        const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchInput.value}`)
+        const shows = res.data
+        searchResults.innerHTML = ''
+        for (show of shows) {
+            const newImg = document.createElement('img')
+            searchResults.append(newImg)
+            newImg.src = `${show.show.image.medium}`
+        }
     }
     catch (error) {
-        console.log(error)
+        console.log(`Error: ${error}`)
     }
-
 }
 
-fetchTv()
+//DOM Event listeners
+
+searchShowForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    GetTvShows(searchInput.value)
+
+    featuredSection.classList.add('hidden')
+    premiereSection.classList.add('hidden')
+})
