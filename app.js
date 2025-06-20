4c93f6ea5092906ca768606b2916c3369d15d2e4
pablothecoder <ortegaperaltap@gmail.com>
2025-06-20 15:59:09 -0400

    featured shows html css and js work

--- app.js
+++ app.js
@@ -6,24 +6,51 @@ const searchShowBtn = document.querySelector('#searchBtn')
 const searchInput = document.querySelector('#searchShowInput')
 const mainContainer = document.querySelector('.main-container')
 const featuredSection = document.querySelector('.featured-today')
-const featuredSectionShows = document.querySelector('featured-today-shows')
+const featuredSectionShows = document.querySelector('#featuredTodayShows')
 const searchResultSection = document.querySelector('search-results')
 const searchResults = document.querySelector('#searchResultShows')
 const searchResultsTitle = document.querySelector('.search-results h2')
 const premiereSection = document.querySelector('.premieres-annoucements')
 
-// TV API functions
+// featured shows function
 
 const displayFeaturedShows = async () => {
     try {
         const res = await axios.get('https://api.tvmaze.com/shows?page=1')
-        console.log(res)
+        const featuredShows = res.data
+        for (let i = 0; i < 7; i++) {
+
+            const newDiv = document.createElement('div')
+            const ratingDiv = document.createElement('div')
+            const newImg = document.createElement('img')
+            const showTitle = document.createElement('a')
+            const showRating = document.createElement('p')
+
+            newDiv.classList.add('result-show')
+            showTitle.classList.add('result-show-name')
+            newImg.classList.add('result-show-image')
+            ratingDiv.classList.add('result-show-rating')
+            showRating.classList.add('result-show-rating-text')
+
+            featuredSectionShows.append(newDiv)
+            newDiv.append(newImg)
+            newDiv.append(showTitle)
+            newDiv.append(ratingDiv)
+            ratingDiv.append(showRating)
+
+            newImg.src = `${featuredShows[i].image.medium}`
+            showTitle.innerText = featuredShows[i].name
+            showTitle.href = featuredShows[i].officialSite
+            showRating.innerHTML = `<p><i class = "fa-solid fa-star"></i>${featuredShows[i].rating.average}</p>`
+        }
     } catch (error) {
         console.log(error)
     }
 }
 
 displayFeaturedShows()
+
+// Search shows functions
 
 const searchTvShows = async (searhInput) => {
     try {
@@ -35,25 +62,25 @@ const searchTvShows = async (searhInput) => {
             const newDiv = document.createElement('div')
             const ratingDiv = document.createElement('div')
             const newImg = document.createElement('img')
-            const newTitle = document.createElement('a')
+            const showTitle = document.createElement('a')
             const showRating = document.createElement('p')
 
             newDiv.classList.add('result-show')
-            newTitle.classList.add('result-show-name')
+            showTitle.classList.add('result-show-name')
             newImg.classList.add('result-show-image')
             ratingDiv.classList.add('result-show-rating')
             showRating.classList.add('result-show-rating-text')
 
             searchResults.append(newDiv)
             newDiv.append(newImg)
-            newDiv.append(newTitle)
+            newDiv.append(showTitle)
             newDiv.append(ratingDiv)
             ratingDiv.append(showRating)
 
 
             newImg.src = `${show.show.image.medium}`
-            newTitle.innerText = show.show.name
-            newTitle.href = show.show.officialSite
+            showTitle.innerText = show.show.name
+            showTitle.href = show.show.officialSite
             showRating.innerHTML = `<p><i class = "fa-solid fa-star"></i>${show.show.rating.average}</p>`
 
         }
@@ -63,7 +90,7 @@ const searchTvShows = async (searhInput) => {
     }
 }
 
-//DOM Event listeners
+// Event listeners
 
 searchShowForm.addEventListener('submit', (e) => {
     e.preventDefault()
