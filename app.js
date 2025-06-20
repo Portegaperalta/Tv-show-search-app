b2513edfbf554df3f91ef21e7662786136c420ba
pablothecoder <ortegaperaltap@gmail.com>
2025-06-20 16:24:39 -0400

    html css and js files updated

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const searchShowForm = document.querySelector('#headerSearchForm')
 const searchShowBtn = document.querySelector('#searchBtn')
 const searchInput = document.querySelector('#searchShowInput')
 const mainContainer = document.querySelector('.main-container')
-const featuredSection = document.querySelector('.featured-today')
+const featuredSection = document.querySelector('#featuredToday')
 const featuredSectionShows = document.querySelector('#featuredTodayShows')
-const searchResultSection = document.querySelector('search-results')
+const searchResultSection = document.querySelector('#searchResults')
 const searchResults = document.querySelector('#searchResultShows')
 const searchResultsTitle = document.querySelector('.search-results h2')
-const premiereSection = document.querySelector('.premieres-annoucements')
+const premiereSection = document.querySelector('#premieresAnnoucements')
+const premiereSectionShows = document.querySelector('#premieresAnnoucementsShows')
 
 // featured shows function
 
 const displayFeaturedShows = async () => {
     try {
-        const res = await axios.get('https://api.tvmaze.com/shows?page=1')
+        const res = await axios.get('https://api.tvmaze.com/shows?page=2')
         const featuredShows = res.data
         for (let i = 0; i < 7; i++) {
 
@@ -49,6 +50,42 @@ const displayFeaturedShows = async () => {
 }
 
 displayFeaturedShows()
+
+const displayPremiereShows = async () => {
+    try {
+        const res = await axios.get('https://api.tvmaze.com/shows?page=4')
+        const premiereShows = res.data
+        for (let i = 0; i < 7; i++) {
+
+            const newDiv = document.createElement('div')
+            const ratingDiv = document.createElement('div')
+            const newImg = document.createElement('img')
+            const showTitle = document.createElement('a')
+            const showRating = document.createElement('p')
+
+            newDiv.classList.add('result-show')
+            showTitle.classList.add('result-show-name')
+            newImg.classList.add('result-show-image')
+            ratingDiv.classList.add('result-show-rating')
+            showRating.classList.add('result-show-rating-text')
+
+            premiereSectionShows.append(newDiv)
+            newDiv.append(newImg)
+            newDiv.append(showTitle)
+            newDiv.append(ratingDiv)
+            ratingDiv.append(showRating)
+
+            newImg.src = `${premiereShows[i].image.medium}`
+            showTitle.innerText = premiereShows[i].name
+            showTitle.href = premiereShows[i].officialSite
+            showRating.innerHTML = `<p><i class = "fa-solid fa-star"></i>${premiereShows[i].rating.average}</p>`
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+displayPremiereShows()
 
 // Search shows functions
 
@@ -95,6 +132,7 @@ const searchTvShows = async (searhInput) => {
 searchShowForm.addEventListener('submit', (e) => {
     e.preventDefault()
     searchTvShows(searchInput.value)
+    searchResultSection.classList.remove('hidden')
     searchResultsTitle.innerHTML = `Search: ${searchInput.value}`
     featuredSection.classList.add('hidden')
     premiereSection.classList.add('hidden')
